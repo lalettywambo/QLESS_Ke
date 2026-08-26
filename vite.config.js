c6c289eb248314f26f15e037fb98ce5f20375058
lalettywambo <lalettywambo@gmail.com>
2026-08-26 22:26:34 +0300

    add code content to business.js file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // (or vue(), svelte(), etc.)
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
